e9b9cb662612b89c5659cf632017cc56355e3e34
Martín Comito <martin@cohley.com>
2021-07-01 21:45:14 -0300

    v-if y v-for

---
+++ src/components/ContenidoExplicito.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- v-if sirve para evaluar cualquier expresión o variable que queramos, y con eso mostramos o no cierta info, en este caso con la prop edad que viene desde el lugar donde se llama el componente vamos a saber si soy mayor o menor -->
+    <p v-if="edad >= 18">Sos mayor de edad</p>
+    <p v-if="edad < 18">Sos menor, rajá de acá</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    edad: Number
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ListaDeBotones.vue
@@ -0,0 +1,38 @@
+
+<template>
+  <div>
+    <!-- Por cada elemento en la prop botones va a renderizar (mostrar) un botón con su título y mensaje cuando se haga click -->
+    <button 
+      class="separado"
+      v-for="elemento in botones"
+      :key="elemento.key"
+      @click="mostrarMensaje(elemento.mensaje)"
+    >
+      {{ elemento.titulo }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Inicio",
+  // ! Prop es todo lo que me llega desde afuera cuando usan este componente en otro lado. En este caso sabemos que es un array que se llama botones, si mando otra cosa que no sea un array me va a decir que el tipo de prop es incorrecto.
+  props: {
+    botones: Array
+  },
+  // ! Methods = Funciones
+  // ! Esta función recibe un mensaje y lo muestra en el alert, cuando hacemos click porque está más arriba con el @click.
+  methods: {
+    mostrarMensaje (unMensaje) {
+      alert(unMensaje)
+    }
+  }
+};
+</script>
+
+<style scoped>
+ .separado {
+   margin-top: 4rem;
+   margin-right: 2rem;
+ }
+</style>
